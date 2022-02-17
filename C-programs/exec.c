67dcf68cb388b595f07ae8dc9711923bb1da178d
Turyal Neeshat <70485527+Altruy@users.noreply.github.com>
2022-02-17 16:03:30 +0500

    Add files via upload

--- C-programs/exec.c
+++ C-programs/exec.c
@@ -1,35 +1,31 @@
 #include <stdio.h>
+#include <unistd.h>
 #include<sys/vfs.h>
 #include <sys/statfs.h>
 #include <unistd.h>
 #include <sys/syscall.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include<sys/socket.h> 
 #include <fcntl.h>
-#include <string.h>
+#define _GNU_SOURCE 
 int main(){
+char old_name1[] = "file1.txt";
 int count =0;
-sleep(5);
 while(1)
 {
-	if(count>=10000)
+	if(count>=1000000)
 		break;
-int value;
-value = rename("file1.txt", "file2.txt");
-value = rename("file2.txt", "file1.txt");
+	char new_name1[] = "file2.txt";
+	int value;
+	value = rename(old_name1, new_name1);
+	strcpy(new_name1,"file1.txt");
+	value = rename(old_name1, new_name1);
+	strcpy(old_name1,new_name1);
 
-syscall(SYS_sync);
-syscall(SYS_getpid);
-syscall(SYS_gettid);
-int socket_desc;
-socket_desc = socket(AF_INET , SOCK_STREAM , 0);
-close(socket_desc);
-// syscall(SYS_pipe2,20000);
-// syscall(SYS_signalfd);
-count=count+1;
-sleep(0.0001);	
+	count=count+1;
+	sleep(0.0001);	
 }
 ;
 return 0;
 }
+
